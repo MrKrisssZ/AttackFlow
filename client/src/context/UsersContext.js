342ebc00914a10069ed9fda63201fdc95925e814
Ruby Chan <a1865524@adelaide.edu.au>
2023-08-28 20:45:12 +0930

    add useContext to update the page automatically

---
+++ client/src/context/UsersContext.js
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from 'react'
+
+export const UsersContext = createContext()
+
+// takes 2 arguments, state: prevState, action: object pass to dispatch function
+export const usersReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_USERS':
+            return {
+                users: action.payload
+            }
+        case 'CREATE_USER':
+            return {
+                users: [action.payload, ...state.users]
+            }
+        default:
+            return state
+    }
+}
+
+export const UsersContextProvider = ({ children }) => {
+    // dispatch takes 2 arguments, type (state change) and payload (data)
+    const [ users, dispatch ] = useReducer(usersReducer, {
+        users: null
+    })
+   
+    return (
+        <UsersContext.Provider value={{ ...users, dispatch }}>
+             { children }
+        </UsersContext.Provider>
+    )
+}
