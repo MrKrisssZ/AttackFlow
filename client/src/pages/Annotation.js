7411cbdf86c973d2cf0ae6cb4ec71f19309b4887
Ruby Chan <a1865524@adelaide.edu.au>
2023-08-31 19:20:56 +0930

    create file upload page for annotation

---
+++ client/src/pages/Annotation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components
+import FileUpload from "../components/FileUpload";
+
+const Annotation = () => {
+    return (
+        <>
+            {/* <h1>Annotation</h1> */}
+            <FileUpload></FileUpload>
+        </>
+    )
+}
+
+export default Annotation
