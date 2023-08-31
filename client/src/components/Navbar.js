2475b0998081367a66519a3e36e26b8d1ded4913
tiffany <ting11222001@gmail.com>
2023-08-31 22:34:30 +0930

    fix: 🐛 update navbar css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    
+
     return (
-        <header>
-            <div className="container">
-                <Link to="/">
-                    <h1>Attack Flow</h1>
+        <header class="bg-white">
+            <div class="max-w-7xl mx-auto p-5 flex items-center justify-between">
+                <Link to="/" class="text-black no-underline">
+                    <h1 class="text-4xl font-bold">Attack Flow</h1>
                 </Link>
-                <Link to='/annotation'>
-                    <h1>Annotation</h1>
+                <Link to="/annotation" class="text-black no-underline">
+                    <h1 class="text-4xl font-bold">Annotation</h1>
                 </Link>
             </div>
         </header>
+
     )
 }
 
