c40c29227d33783a14639963cd23be25a78bbe89
Danby <pigisdog@DanbydeMacBook-Air.local>
2023-09-06 23:52:28 +0930

    annotation file modified

--- client/src/pages/Annotation.js
+++ client/src/pages/Annotation.js
@@ -1,13 +1,17 @@
-// components
-import ReportUploadForm from '../components/ReportForm'
+import React from 'react';
+import ReportUploadForm from '../components/ReportForm';
+import FileUpload from '../FileUpload'; // 根据文件路径进行调整
 
 const Annotation = () => {
     return (
         <>
             {/* <h1>Annotation</h1> */}
-            <ReportUploadForm></ReportUploadForm>
+            <ReportUploadForm />
+            
+            {/* 这里添加文件上传组件 */}
+            <FileUpload />
         </>
-    )
+    );
 }
 
-export default Annotation
+export default Annotation;
