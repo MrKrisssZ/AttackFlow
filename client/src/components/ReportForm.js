0497725e583631d01f6f22e707cf1db16956b422
Yiwei Shen <a1829951@student.adelaide.edu.au>
2023-09-06 14:59:47 +0930

    add a message modal when uploading a url

--- client/src/components/ReportForm.js
+++ client/src/components/ReportForm.js
@@ -1,58 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import MessageModal from "./MessageModal.js";
 
 const ReportForm = () => {
-    // To-do: get the userID
-    const [ userID, setUserID ] = useState('user01')
-    const [ url, setUrl] = useState('')
-    const [ uploadedAt, setUploadedAt] = useState(Date())
-    const [ validated, setValidated] = useState(false)
+  // To-do: get the userID
+  const [userID, setUserID] = useState("user01");
+  const [url, setUrl] = useState("");
+  const [uploadedAt, setUploadedAt] = useState(Date());
+  const [validated, setValidated] = useState(false);
 
-    const [ error, setError ] = useState(null)
+  const [error, setError] = useState(null);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const report = { url, uploadedAt, userID, validated }
+    const report = { url, uploadedAt, userID, validated };
 
-        const response = await fetch('/api/reports', {
-            method: 'POST',
-            body: JSON.stringify(report),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+    const response = await fetch("/api/reports", {
+      method: "POST",
+      body: JSON.stringify(report),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        const json = await response.json()
+    const json = await response.json();
 
-        if (!response.ok) {
-            setError(json.error)
-        }
-
-        if (response.ok) {
-            // reset the error
-            setError(null)
-
-            //reset the form
-            setUrl('')
-
-            console.log('new report added: ', json)
-            // dispatch({ type: 'CREATE_REPORT ', payload: json })
-        }
+    if (!response.ok) {
+      setError(json.error);
+      setModalMessage("Upload failed");
     }
 
-    return (
-        <form className='create' onSubmit={ handleSubmit }>
-            <h3>Upload an incident report link</h3>
-                <label>Enter the url</label>
-                <input 
-                    type='url' 
-                    onChange={(e) => setUrl(e.target.value)}
-                    pattern='https://.*' 
-                    required />
-                <button>Upload</button>
-                { error && <div className='error'>{ error }</div>}
-        </form>
-    )
-}
+    if (response.ok) {
+      // reset the error
+      setError(null);
+      setModalMessage("Upload successfully");
 
-export default ReportForm
+      //reset the form
+      setUrl("");
+
+      console.log("new report added: ", json);
+      // dispatch({ type: 'CREATE_REPORT ', payload: json })
+    }
+
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Upload an incident report link</h3>
+      <label>Enter the url</label>
+      <input
+        type="url"
+        onChange={(e) => setUrl(e.target.value)}
+        pattern="https://.*"
+        required
+      />
+      <button>Upload</button>
+      <MessageModal
+        isOpen={isModalOpen}
+        message={modalMessage}
+        onClose={closeModal}
+      />
+    </form>
+  );
+};
+
+export default ReportForm;
+
+//{ error && <div className='error'>{ error }</div>}
