a460eeeeabc6f6415c1ab9ca1dbbbaf49cf9f25d
tiffany <ting11222001@gmail.com>
2023-09-06 21:34:51 +0930

    fix: 🐛 update report details css to tailwind

--- client/src/components/ReportDetails.js
+++ client/src/components/ReportDetails.js
@@ -4,22 +4,24 @@ const ReportDetails = ({ report }) => {
     const { dispatch } = useReportsContext()
 
     const handleClick = async () => {
-        const response = await fetch('/api/reports/'+report._id, {
+        const response = await fetch('/api/reports/' + report._id, {
             method: 'DELETE'
         })
         const json = await response.json()
 
         if (response.ok) {
-            dispatch({ type: 'DELETE_REPORT', payload: json})
+            dispatch({ type: 'DELETE_REPORT', payload: json })
         }
     }
     return (
-        <div className='user-details'>
-            <p><strong>URL: </strong>{ report.url }</p>
-            <p><strong>Reported by: </strong>{ report.userID }</p>
-            <p><strong>Reported at: </strong>{ report.uploadedAt }</p>
-            <span onClick={ handleClick }>delete</span>
-        </div>
+        <div className="bg-white rounded-lg my-5 mx-auto p-5 relative shadow-md">
+            <p className="text-lg font-semibold mb-2"><strong>URL: </strong>{report.url}</p>
+            <p className="text-sm text-gray-600"><strong>Reported by: </strong>{report.userID}</p>
+            <p className="text-sm text-gray-600"><strong>Reported at: </strong>{report.uploadedAt}</p>
+            <div class="flex justify-center rounded-md bg-gray w-24 mt-4">
+                <span onClick={handleClick} className="cursor-pointer bg-gray-200 rounded-full text-gray-600">delete</span>
+            </div>
+        </div >
     )
 }
 
