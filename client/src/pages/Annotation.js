76484d11c70f3badfec5578aa2a75b460f840793
Ruby Chan <a1865524@adelaide.edu.au>
2023-09-06 23:20:11 +0930

    create dropdown menu for annotation

--- client/src/pages/Annotation.js
+++ client/src/pages/Annotation.js
@@ -1,11 +1,13 @@
 // components
 import ReportUploadForm from '../components/ReportForm'
+import DropdownMenu from '../components/DropDownMenu'
 
 const Annotation = () => {
     return (
         <>
             {/* <h1>Annotation</h1> */}
             <ReportUploadForm></ReportUploadForm>
+            <DropdownMenu></DropdownMenu>
         </>
     )
 }
