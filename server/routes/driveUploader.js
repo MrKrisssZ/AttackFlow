5c7d8b535053bfde5762668085446638922f052e
Danby <pigisdog@DanbydeMacBook-Air.local>
2023-09-19 14:45:09 +0930

    Modified driveUploader and removed unintended file

--- server/routes/driveUploader.js
+++ server/routes/driveUploader.js
@@ -2,12 +2,15 @@ const { google } = require('googleapis');
 const serviceAccount = require('../serviceAccountKey.json');
 const { Readable } = require('stream');
 
+// Google client details
 const CLIENT_ID = serviceAccount.client_id;
 const CLIENT_EMAIL = serviceAccount.client_email;
 const PRIVATE_KEY = serviceAccount.private_key;
 
+// Define necessary Google Drive scopes
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
+// Authenticate using the service account
 const auth = new google.auth.JWT(
     CLIENT_EMAIL,
     null,
@@ -16,17 +19,21 @@ const auth = new google.auth.JWT(
     null
 );
 
+// Initialize Google Drive API
 const drive = google.drive({ version: 'v3', auth });
 
+// Define the upload function
 const uploadToDrive = async (fileName, fileBuffer, mimeType = 'application/pdf') => {
     try {
+        // Create buffer stream from file buffer
         const bufferStream = new Readable();
         bufferStream.push(fileBuffer);
         bufferStream.push(null);
 
-        // Folder ID where you want to upload the file
+        // Folder ID target for the upload
         const folderId = '1NNIzWvBy0xS9-00jImN24KsYcU2KeSX9';
 
+        // Start the upload process
         const response = await drive.files.create({
             requestBody: {
                 name: fileName,
@@ -39,8 +46,9 @@ const uploadToDrive = async (fileName, fileBuffer, mimeType = 'application/pdf')
             }
         });
 
+        // Check if the upload was successful
         if (response.data && response.data.id) {
-            // Grant read permissions for the uploaded file to a specific email
+            // If successful, grant read permissions for the uploaded file to a specific email
             await drive.permissions.create({
                 fileId: response.data.id,
                 requestBody: {
@@ -49,14 +57,18 @@ const uploadToDrive = async (fileName, fileBuffer, mimeType = 'application/pdf')
                     emailAddress: 'attackflow15@gmail.com'
                 }
             });
+            
             return response.data;
         } else {
+            // Handle failed upload
             throw new Error('Failed to upload the file.');
         }
     } catch (error) {
+        // Error logging and propagation
         console.error('Error uploading file:', error);
         throw new Error(`Error uploading file: ${error.message}`);
     }
 }
 
+// Export necessary modules
 module.exports = { uploadToDrive, auth };
