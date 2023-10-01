7eda76ddfc91ea7be1c4d74c5092a91441ce0cba
Ruby Chan <a1865524@adelaide.edu.au>
2023-10-01 20:41:41 +1030

    move the constants for the dropdown menu into new files and organized into folder

---
+++ client/src/constants/ICS.tsx
@@ -0,0 +1,144 @@
+// categories -> techniques by different tactics
+// since same techniques under different tactics with different techID
+// To-do: complete the dictionary for all categories with techniques
+export const techniqueByCategoriesICS = {
+    InitialAccess: [
+      "Techniques",
+      "Drive-by Compromise",
+      "Exploit Public-Facing Application",
+      "Exploitation of Remote Services",
+      "External Remote Services",
+      "Internet Accessible Device",
+      "Remote Services",
+      "Replication Through Removable Media",
+      "Rogue Master",
+      "Spearphishing Attachment",
+      "Supply Chain Compromise",
+      "Transient Cyber Asset",
+      "Wireless Compromise",
+    ],
+  
+    Execution: [
+      "Techniques",
+      "Change Operating Mode",
+      "Command-Line Interface",
+      "Execution through API",
+      "Graphical User Interface",
+      "Hooking",
+      "Modify Controller Tasking",
+      "Native API",
+      "Scripting",
+      "User Execution",
+    ],
+  
+    Persistence: [
+      "Techniques",
+      "Hardcoded Credentials",
+      "Modify Program",
+      "Module Firmware",
+      "Project File Infection",
+      "System Firmware",
+      "Valid Accounts",
+    ],
+  
+    PrivilegeEscalation: [
+      "Techniques",
+      "Exploitation for Privilege Escalation",
+      "Hooking",
+    ],
+  
+    Evasion: [
+      "Techniques",
+      "Change Operating Mode",
+      "Exploitation for Evasion",
+      "Indicator Removal on Host",
+      "Masquerading",
+      "Rootkit",
+      "Spoof Reporting Message",
+    ],
+  
+    Discovery: [
+      "Techniques",
+      "Network Connection Enumeration",
+      "Network Sniffing",
+      "Remote System Discovery",
+      "Remote System Information Discovery",
+      "Wireless Sniffing",
+    ],
+  
+    LateralMovement: [
+      "Techniques",
+      "Default Credentials",
+      "Exploitation of Remote Services",
+      "Hardcoded Credentials",
+      "Lateral Tool Transfer",
+      "Program Download",
+      "Remote Services",
+      "Valid Accounts",
+    ],
+  
+    Collection: [
+      "Techniques",
+      "Adversary-in-the-Middle",
+      "Automated Collection",
+      "Data from Information Repositories",
+      "Data from Local System",
+      "Detect Operating Mode",
+      "I/O Image",
+      "Monitor Process State",
+      "Point & Tag Identification",
+      "Program Upload",
+      "Screen Capture",
+      "Wireless Sniffing",
+    ],
+  
+    CommandAndControl: [
+      "Techniques",
+      "Commonly Used Port",
+      "Connection Proxy",
+      "Standard Application Layer Protocol",
+    ],
+  
+    InhibitResponseFunction: [
+      "Techniques",
+      "Activate Firmware Update Mode",
+      "Alarm Suppression",
+      "Block Command Message",
+      "Block Reporting Message",
+      "Block Serial COM",
+      "Change Credential",
+      "Data Destruction",
+      "Denial of Service",
+      "Device Restart/Shutdown",
+      "Manipulate I/O Image",
+      "Modify Alarm Settings",
+      "Rootkit",
+      "Service Stop",
+      "System Firmware",
+    ],
+  
+    ImpairProcessControl: [
+      "Techniques",
+      "Brute Force I/O",
+      "Modify Parameter",
+      "Module Firmware",
+      "Spoof Reporting Message",
+      "Unauthorized Command Message",
+    ],
+  
+    Impact: [
+      "Techniques",
+      "Damage to Property",
+      "Denial of Control",
+      "Denial of View",
+      "Loss of Availability",
+      "Loss of Control",
+      "Loss of Productivity and Revenue",
+      "Loss of Protection",
+      "Loss of Safety",
+      "Loss of View",
+      "Manipulation of Control",
+      "Manipulation of View",
+      "Theft of Operational Information",
+    ],
+  };
