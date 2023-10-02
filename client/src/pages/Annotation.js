12d1a41fb10fa18167936fa87945c82d582941ef
Ruby Chan <a1865524@adelaide.edu.au>
2023-10-02 11:09:57 +1030

    pass the description from the ReportFrom to DropDownMenu

--- client/src/pages/Annotation.js
+++ client/src/pages/Annotation.js
@@ -1,24 +1,36 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAuthContext } from "../hooks/UseAuthContext";
 import { Link, useLocation } from "react-router-dom";
-import ReportForm from "../components/ReportForm";
 
 // components
 import DropdownMenu from "../components/DropDownMenu";
+import ReportForm from "../components/ReportForm";
 
 const Annotation = () => {
   const { user } = useAuthContext();
   const location = useLocation();
+
+  // writing annotation file
+  const [ descFromReport, setDescFromReport ] = useState('')
+
+  const handleReportData = (reportData) => {
+    setDescFromReport(reportData)
+    // console.log('report data from child',descFromReport)
+  }
+
+  useEffect(() => {
+    console.log('descFromReport useEffect', descFromReport);
+  }, [descFromReport]);
 
   return (
     <div style={{ display: 'flex' }}>
       {user && (
         <>
           <div style={{ flex: 7, marginRight: "1rem" }}>
-            <ReportForm></ReportForm>
+            <ReportForm sendDescToParent={handleReportData}></ReportForm>
           </div>
           <div style={{ flex: 3 }}>
-            <DropdownMenu></DropdownMenu>
+            <DropdownMenu desc={descFromReport}></DropdownMenu>
           </div>
         </>
       )}
