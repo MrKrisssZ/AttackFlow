62e50e3b71bd5da742dac98cf824541552db1e91
Yiwei Shen <a1829951@student.adelaide.edu.au>
2023-10-07 17:15:35 +1030

    Embed the visualization tool to the platform

--- client/src/pages/Visualization.js
+++ client/src/pages/Visualization.js
@@ -1,14 +1,12 @@
 const Visualization = () => {
-    return (
-        <>
-            <h3 className="text-lg font-semibold mb-4">Visualization Section</h3>
+  return (
+    <>
+      <h3 className="text-lg font-semibold mb-4">Visualization Section</h3>
+      <main>
+        <iframe src="https://center-for-threat-informed-defense.github.io/attack-flow/ui/"></iframe>
+      </main>
+    </>
+  );
+};
 
-
-            <main>
-
-            </main>
-        </>
-    )
-}
-
-export default Visualization
+export default Visualization;
