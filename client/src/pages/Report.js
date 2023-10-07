6da16f49b6c741280f75675b0178996407bb6b7c
tiffany <ting11222001@gmail.com>
2023-10-07 11:34:42 +1030

    feat: 🎸 c4806eb report page

--- client/src/pages/Report.js
+++ client/src/pages/Report.js
@@ -30,7 +30,7 @@ const Report = () => {
             <div>
                 <h3 className="text-lg font-semibold mb-4">Report</h3>
                 {reports && reports.map((report) => (
-                    <ReportDetails key={report._id} report={report} />
+                    <ReportDetails key={report._id} report={report} validated={report.validated} />
                 ))}
             </div>
         </div >
