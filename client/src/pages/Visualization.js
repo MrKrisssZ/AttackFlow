b50ae3cd4bfd7128b993165d5318b151becf108d
tiffany <ting11222001@gmail.com>
2023-10-07 11:35:46 +1030

    feat: 🎸 c4806eb visualization page

--- client/src/pages/Visualization.js
+++ client/src/pages/Visualization.js
@@ -1,14 +1,11 @@
-// testing for intergrating ChatGPT
-import ChatGPT from "../components/ChatGPT"
-
 const Visualization = () => {
     return (
         <>
             <h3 className="text-lg font-semibold mb-4">Visualization Section</h3>
-            {/* testing for intergrating ChatGPT */}
-            <h3 className="text-lg font-semibold mb-4">ChatGPT Integration</h3>
+
+
             <main>
-                <ChatGPT />
+
             </main>
         </>
     )
