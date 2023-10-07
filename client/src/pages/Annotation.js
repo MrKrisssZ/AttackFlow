b2748e646c7e11dfe5b1d21dab9ce9b3f00bf529
tiffany <ting11222001@gmail.com>
2023-10-07 11:33:21 +1030

    feat: 🎸 c4806eb annotation page

--- client/src/pages/Annotation.js
+++ client/src/pages/Annotation.js
@@ -1,13 +1,36 @@
+import { useState, useEffect } from "react";
+import { useAuthContext } from "../hooks/UseAuthContext";
+import { Link, useLocation } from "react-router-dom";
+import ReportForm from "../components/ReportForm";
+
 // components
-import ReportUploadForm from '../components/ReportForm'
+import DropdownMenu from "../components/DropDownMenu";
 
 const Annotation = () => {
+    const { user } = useAuthContext();
+    const location = useLocation();
+
     return (
         <>
-            {/* <h1>Annotation</h1> */}
-            <ReportUploadForm></ReportUploadForm>
+            {user && (
+                <>
+                    <ReportForm></ReportForm>
+                    <DropdownMenu></DropdownMenu>
+                </>
+            )}
+            {!user && (
+                <>
+                    <h3 className="text-lg font-semibold mb-4">Not signed in</h3>
+                    <p>
+                        <Link to="/login" state={{ redirectTo: location }}>
+                            <button className="text-primary">Login</button>
+                        </Link>{" "}
+                        to use the annotation function.
+                    </p>
+                </>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default Annotation
+export default Annotation;
