a8e77c5049b70096b02bc13d63bfc138b51720aa
Yiwei Shen <a1829951@student.adelaide.edu.au>
2023-10-07 17:19:37 +1030

    Resize the visualization tool

--- client/src/pages/Visualization.js
+++ client/src/pages/Visualization.js
@@ -3,7 +3,11 @@ const Visualization = () => {
     <>
       <h3 className="text-lg font-semibold mb-4">Visualization Section</h3>
       <main>
-        <iframe src="https://center-for-threat-informed-defense.github.io/attack-flow/ui/"></iframe>
+        <iframe
+          src="https://center-for-threat-informed-defense.github.io/attack-flow/ui/"
+          width="100%"
+          height="500px"
+        ></iframe>
       </main>
     </>
   );
