f7bd739ee52f01bc75d78810ef45081664376926
Yiwei Shen <a1829951@student.adelaide.edu.au>
2023-10-09 16:16:42 +1030

    Merge branch 35 and 41 into 42

--- client/src/pages/Visualization.js
+++ client/src/pages/Visualization.js
@@ -1,14 +1,16 @@
 const Visualization = () => {
-    return (
-        <>
-            <h3 className="text-lg font-semibold mb-4">Visualization Section</h3>
+  return (
+    <>
+      <h3 className="text-lg font-semibold mb-4">Visualization Section</h3>
+      <main>
+        <iframe
+          src="https://center-for-threat-informed-defense.github.io/attack-flow/ui/"
+          width="100%"
+          height="500px"
+        ></iframe>
+      </main>
+    </>
+  );
+};
 
-
-            <main>
-
-            </main>
-        </>
-    )
-}
-
-export default Visualization
+export default Visualization;
