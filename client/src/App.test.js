2a47c13a91214454331ede73b8066cba9ede1b4b
tiffany <ting11222001@gmail.com>
2023-10-14 13:40:46 +1030

    feat: 🎸 test header text display before users log in

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import { AuthContextProvider } from './context/AuthContext';
+import { ReportsContextProvider } from './context/ReportsContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+const customRender = (ui) => {
+  return render(
+    <React.StrictMode>
+      <AuthContextProvider>
+        <ReportsContextProvider>
+          {ui}
+        </ReportsContextProvider>
+      </AuthContextProvider>
+    </React.StrictMode>
+  );
+};
+
+
+
+// check header before log in
+test('when users are not log in, show attack flow in the header', () => {
+  customRender(<App />);
+  const linkElement = screen.getByText(/attack flow/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('when users are not log in, show annotation in the header', () => {
+  customRender(<App />);
+  const linkElement = screen.getByText(/annotation/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('when users are not log in, show visualization in the header', () => {
+  customRender(<App />);
+  const linkElement = screen.getByText(/visualization/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('when users are not log in, do not show report in the header', () => {
+  customRender(<App />);
+  const linkElement = screen.queryByText(/reoprt/i);
+  expect(linkElement).toBeNull();
 });
